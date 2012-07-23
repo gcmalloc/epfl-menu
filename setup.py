5584e5730fb2b714f2185dc21c2d48cdfb64c4b2
Maxime Augier <max@xolus.net>
2012-07-23 18:42:09 +0200

    Fix typo

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(name='epfl-menu',
       version = '0.1',
-      description = 'File checksumming utility',
+      description = 'Retrieve EPFL menus of the day',
       author = 'gcmalloc',
       url = 'http://github.com/gcmalloc/epfl-menu',
       py_modules = ['epfl.menu'],
